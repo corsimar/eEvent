6c104e6d13c2b8aa7dbf47a98ac034c12d498ea2
Tălpău Mircea Cristian <93196927+MirceaTalpau@users.noreply.github.com>
2023-01-08 14:04:57 +0200

    am adaugat package proiect

---
+++ src/proiect/Utils.java
@@ -0,0 +1,64 @@
+package proiect;
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Utils implements Serializable {
+    public static ArrayList<String> preference = new ArrayList<String>(){{
+        
+        add("Arta");
+        add("Parenting");
+        add("Antreprenoriat");
+        add("Dezvoltare_Personala");
+        add("Dating,Petreceri");
+        add("Natura");
+        add("Sanatate");
+        add("Psihologie");
+        add("Sport");
+        add("Tehnologie");
+        add("Film");
+    }};
+    public static ArrayList<String> location = new ArrayList<String>(){{    
+        add("Alba");
+        add("Arad");
+        add("Arges");
+        add("Bacau");
+        add("Bihor");
+        add("Bistrita_Nasaud");
+        add("Botosani");
+        add("Brasov");
+        add("Braila");
+        add("Buzau");
+        add("Caras_Severin,");
+        add("Calarasi ");
+        add("Cluj ");
+        add("Constanta ");
+        add("Covasna ");
+        add("Dambovita ");
+        add("Dolj ");
+        add("Galati");
+        add("Giurgiu");
+        add("Gorj ");
+        add("Harghita ");
+        add("Hunedoara ");
+        add("Ialomita ");
+        add("Iasi ");
+        add("Ilfov ");
+        add("Maramures");
+        add("Mehedinti ");
+        add("Mures ");
+        add("Neamt ");
+        add("Olt ");
+        add("Prahova");
+        add("Satu_Mare");
+        add("Salaj ");
+        add("Sibiu ");
+        add("Suceava");
+        add("Teleorman");
+        add("Timis ");
+        add("Tulcea ");
+        add("Vaslui ");
+        add("Valcea ");
+        add("Vrancea ");
+    }};
+    
+}
